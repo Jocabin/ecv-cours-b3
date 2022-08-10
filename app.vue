03c530979b4b30bcab7b4c306d1952f570fac14e
Jocabin <kobyfar@hotmail.com>
2022-08-10 13:50:40 +0200

    clean some code, need to fix title template

--- app.vue
+++ app.vue
@@ -1,3 +1,10 @@
+<script setup>
+// FIXME duplicate title template
+useHead({
+  titleTemplate: "%s - Support de cours B3",
+});
+</script>
+
 <template>
   <NavBar />
 
--- components/FooterBar.vue
+++ components/FooterBar.vue
@@ -2,16 +2,6 @@
   <footer
     class="w-full border-t border-gray p-4 flex flex-col items-center justify-center gap-y-8"
   >
-    <!-- <nav>
-      <ContentNavigation v-slot="{ navigation }">
-        <ul role="list" aria-label="Second niveau">
-          <li v-for="link of navigation" :key="link._path">
-            <NuxtLink :to="link._path">{{ link.title }}</NuxtLink>
-          </li>
-        </ul>
-      </ContentNavigation>
-    </nav> -->
-
     <address class="text-center not-italic">
       Ce site est maintenu par
       <a class="text-blue" href="https://github.com/Jocabin">Jocabin</a>
